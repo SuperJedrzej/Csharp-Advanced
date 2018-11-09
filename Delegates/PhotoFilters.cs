95eece11ae906e456b110c67f8b50d0b3a4afc07
SuperJedrzej <j.kubiak164@gmail.com>
2018-11-09 18:01:10 +0100

    delegates

---
+++ Delegates/PhotoFilters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class PhotoFilters
+    {
+        public PhotoFilters()
+        {
+        }
+
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("apply brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("apply contrast");
+        }
+
+        public void Resize(Photo photo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+          {
+            var processor = new PhotoProcessor();
+            var filters  =new PhotoFilters();
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            //filterHandler += filters.RemoveRedEveFilter;
+
+             processor.Process("photo.jpg",filterHandler);
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("apply removeredeye");
+        }
+    }
+}
