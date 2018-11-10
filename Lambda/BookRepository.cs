9368fbcff73a7a167d063241a35e7845411ea74d
SuperJedrzej <j.kubiak164@gmail.com>
2018-11-10 22:14:00 +0100

    Lambda Expressions

---
+++ Lambda/BookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Lambda
+{
+    public class BookRepository
+    { 
+
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "TItle 1", Price = 5},
+                new Book() {Title = "TItle 2", Price = 7},
+                new Book() {Title = "TItle 3", Price = 12}
+            };
+        }
+    }
+}
